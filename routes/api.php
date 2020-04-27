42d8c8e23691ef6871d0027741666c1485390580
shoto-hatayama <hatayama.s00045@gmail.com>
2020-04-27 20:52:31 +0900

    APIルーティング設定

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['namespace' => 'Api'], function() {
+    // LineからのWebhookを受信
+    Route::post('/line/webhook', 'LineWebhookController@webhook')->name('line.webhook');
 });
